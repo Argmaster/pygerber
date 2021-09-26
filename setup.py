3d53f40ddd856a03061a9b4047ebfea4d801a530
Argmaster <argmaster.world@gmail.com>
2021-09-26 15:46:54 +0200

    Formatted setup.py with black

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ from setuptools import setup
 
 def read(*names, **kwargs):
     with io.open(
-        join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")
     ) as fh:
         return fh.read()
 
@@ -25,48 +24,57 @@ with open("src/requirements.txt") as file:
     INTALL_REQUIRES = file.readlines()
 
 setup(
-    name='pygerber',
-    version='0.0.0',
-    license='MIT',
-    description='Python package for 2D and 3D rendering of GerberX3 files.',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    name="pygerber",
+    version="0.0.0",
+    license="MIT",
+    description="Python package for 2D and 3D rendering of GerberX3 files.",
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
     ),
-    author='Krzysztof Wiśniewski',
-    author_email='argmaster.world@gmail.com',
-    url='https://github.com/Argmaster/pygerber',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    author="Krzysztof Wiśniewski",
+    author_email="argmaster.world@gmail.com",
+    url="https://github.com/Argmaster/pygerber",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Topic :: Multimedia :: Graphics :: 3D Rendering',
-        'Topic :: Text Processing :: General',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Multimedia :: Graphics :: 3D Rendering",
+        "Topic :: Text Processing :: General",
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Documentation': 'https://pygerber.readthedocs.io/',
-        'Changelog': 'https://pygerber.readthedocs.io/en/latest/changelog.html',
-        'Issue Tracker': 'https://github.com/Argmaster/pygerber/issues',
+        "Documentation": "https://pygerber.readthedocs.io/",
+        "Changelog": "https://pygerber.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/Argmaster/pygerber/issues",
     },
     keywords=[
-        'python-3', 'gerber', 'gerber-rendering', 'gerber-x3', 'python-rendering', 'gerber-parser', 'python-3.9'
+        "python-3",
+        "gerber",
+        "gerber-rendering",
+        "gerber-x3",
+        "python-rendering",
+        "gerber-parser",
+        "python-3.9",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     install_requires=INTALL_REQUIRES,
     extras_require={
         # eg:
@@ -74,8 +82,8 @@ setup(
         #   ':python_version=="2.6"': ['argparse'],
     },
     entry_points={
-        'console_scripts': [
-            'pygerber = pygerber.cli:main',
+        "console_scripts": [
+            "pygerber = pygerber.cli:main",
         ]
     },
 )
