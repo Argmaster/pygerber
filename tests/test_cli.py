f8529d03096e386bf01ca7f90f976090e7981036
Argmaster <argmaster.world@gmail.com>
2021-10-20 13:16:59 +0200

    Update CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,32 +1,85 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-import os
+from pathlib import Path
 from unittest import TestCase, main
+
+from pygerber.cli import handle_pygerber_cli
+
+TEMP_FOLDER = Path("tests/.temp")
+TEMP_FOLDER.mkdir(0o777, True, True)
+save_path = TEMP_FOLDER / "render.png"
 
 
 class TestCLI(TestCase):
-    def test_render_pillow_multi_file(self):
-        save_path = "render.png"
-        try:
-            os.remove(save_path)
-        except Exception:
-            pass
-        value = os.system(
-            f'python -m pygerber --pillow --toml "tests/gerber/pillow/specfile.toml" -s "{save_path}"'
+    def test_render_pillow_toml(self):
+        handle_pygerber_cli(
+            [
+                "--pillow",
+                "--toml",
+                "tests/gerber/pillow/specfile.toml",
+                "-s",
+                "render.png",
+            ]
         )
-        self.assertEqual(value, 0)
 
-    def test_render_blender_multi_file(self):
-        save_path = "render.png"
-        try:
-            os.remove(save_path)
-        except Exception:
-            pass
-        value = os.system(
-            f'python -m pygerber --blender --toml "tests/gerber/blender/specfile.toml" -s "{save_path}" --dry'
+    def test_render_pillow_yaml(self):
+        handle_pygerber_cli(
+            [
+                "--pillow",
+                "--yaml",
+                "tests/gerber/pillow/specfile.yaml",
+                "-s",
+                "render.png",
+            ]
         )
-        self.assertEqual(value, 0)
+
+    def test_render_pillow_json(self):
+        handle_pygerber_cli(
+            [
+                "--pillow",
+                "--json",
+                "tests/gerber/pillow/specfile.json",
+                "-s",
+                "render.png",
+            ]
+        )
+
+    def test_render_blender_toml(self):
+        handle_pygerber_cli(
+            [
+                "--blender",
+                "--toml",
+                "tests/gerber/blender/specfile.toml",
+                "-s",
+                "render.png",
+                "--dry",
+            ]
+        )
+
+    def test_render_blender_yaml(self):
+        handle_pygerber_cli(
+            [
+                "--blender",
+                "--yaml",
+                "tests/gerber/blender/specfile.yaml",
+                "-s",
+                "render.png",
+                "--dry",
+            ]
+        )
+
+    def test_render_blender_json(self):
+        handle_pygerber_cli(
+            [
+                "--blender",
+                "--json",
+                "tests/gerber/blender/specfile.json",
+                "-s",
+                "render.png",
+                "--dry",
+            ]
+        )
 
 
 if __name__ == "__main__":
