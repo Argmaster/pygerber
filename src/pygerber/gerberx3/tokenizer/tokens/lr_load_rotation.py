54c3437f9adab8f50433804a4c8e611b411f4852
Krzysztof Wisniewski <argmaster.world@gmail.com>
2023-07-23 23:30:10 +0200

    Add parser support for mirroring, scaling and rotation

--- src/pygerber/gerberx3/tokenizer/tokens/lr_load_rotation.py
+++ src/pygerber/gerberx3/tokenizer/tokens/lr_load_rotation.py
@@ -2,12 +2,16 @@
 from __future__ import annotations
 
 from decimal import Decimal
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Iterable, Tuple
 
 from pygerber.gerberx3.tokenizer.tokens.token import Token
 
 if TYPE_CHECKING:
     from typing_extensions import Self
+
+    from pygerber.backend.abstract.backend_cls import Backend
+    from pygerber.backend.abstract.draw_actions.draw_action import DrawAction
+    from pygerber.gerberx3.parser.state import State
 
 
 class LoadRotation(Token):
@@ -23,6 +27,22 @@ class LoadRotation(Token):
         """Initialize token object."""
         rotation = Decimal(tokens["rotation"])
         return cls(rotation=rotation)
+
+    def update_drawing_state(
+        self,
+        state: State,
+        _backend: Backend,
+    ) -> Tuple[State, Iterable[DrawAction]]:
+        """Set drawing polarity."""
+        return (
+            state.model_copy(
+                update={
+                    "rotation": self.rotation,
+                },
+                deep=True,
+            ),
+            (),
+        )
 
     def __str__(self) -> str:
         """Return pretty representation of comment token."""
--- src/pygerber/gerberx3/tokenizer/tokens/ls_load_scaling.py
+++ src/pygerber/gerberx3/tokenizer/tokens/ls_load_scaling.py
@@ -2,12 +2,16 @@
 from __future__ import annotations
 
 from decimal import Decimal
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Iterable, Tuple
 
 from pygerber.gerberx3.tokenizer.tokens.token import Token
 
 if TYPE_CHECKING:
     from typing_extensions import Self
+
+    from pygerber.backend.abstract.backend_cls import Backend
+    from pygerber.backend.abstract.draw_actions.draw_action import DrawAction
+    from pygerber.gerberx3.parser.state import State
 
 
 class LoadScaling(Token):
@@ -23,6 +27,22 @@ class LoadScaling(Token):
         """Initialize token object."""
         scaling = Decimal(tokens["scaling"])
         return cls(scaling=scaling)
+
+    def update_drawing_state(
+        self,
+        state: State,
+        _backend: Backend,
+    ) -> Tuple[State, Iterable[DrawAction]]:
+        """Set drawing polarity."""
+        return (
+            state.model_copy(
+                update={
+                    "scaling": self.scaling,
+                },
+                deep=True,
+            ),
+            (),
+        )
 
     def __str__(self) -> str:
         """Return pretty representation of comment token."""
