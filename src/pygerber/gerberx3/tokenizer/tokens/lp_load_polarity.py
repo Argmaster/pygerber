bb9a47b91916c92280ad5d6386ea719c9904010a
Krzysztof Wisniewski <argmaster.world@gmail.com>
2023-07-23 21:11:09 +0200

    Add support for LP command

--- src/pygerber/gerberx3/tokenizer/tokens/lp_load_polarity.py
+++ src/pygerber/gerberx3/tokenizer/tokens/lp_load_polarity.py
@@ -1,13 +1,17 @@
 """Wrapper for load polarity token."""
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING, Any, Iterable, Tuple
 
 from pygerber.gerberx3.state_enums import Polarity
 from pygerber.gerberx3.tokenizer.tokens.token import Token
 
 if TYPE_CHECKING:
     from typing_extensions import Self
+
+    from pygerber.backend.abstract.backend_cls import Backend
+    from pygerber.backend.abstract.draw_actions.draw_action import DrawAction
+    from pygerber.gerberx3.parser.state import State
 
 
 class LoadPolarity(Token):
@@ -23,6 +27,22 @@ class LoadPolarity(Token):
         """Initialize token object."""
         polarity = Polarity(tokens["polarity"])
         return cls(polarity=polarity)
+
+    def update_drawing_state(
+        self,
+        state: State,
+        _backend: Backend,
+    ) -> Tuple[State, Iterable[DrawAction]]:
+        """Set drawing polarity."""
+        return (
+            state.model_copy(
+                update={
+                    "polarity": self.polarity,
+                },
+                deep=True,
+            ),
+            (),
+        )
 
     def __str__(self) -> str:
         """Return pretty representation of comment token."""
