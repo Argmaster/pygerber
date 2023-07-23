8e6f8751fbbefc43ed056d947e90cc2d3a22668f
Krzysztof Wisniewski <argmaster.world@gmail.com>
2023-07-23 20:26:35 +0200

    Add draw mode selection support to parser

--- src/pygerber/gerberx3/tokenizer/tokens/g0n_set_draw_mode.py
+++ src/pygerber/gerberx3/tokenizer/tokens/g0n_set_draw_mode.py
@@ -1,7 +1,15 @@
 """Wrapper for G01 mode set token."""
 from __future__ import annotations
 
+from typing import TYPE_CHECKING, Iterable, Tuple
+
+from pygerber.gerberx3.state_enums import DrawMode
 from pygerber.gerberx3.tokenizer.tokens.token import Token
+
+if TYPE_CHECKING:
+    from pygerber.backend.abstract.backend_cls import Backend
+    from pygerber.backend.abstract.draw_actions.draw_action import DrawAction
+    from pygerber.gerberx3.parser.state import State
 
 
 class SetLinear(Token):
@@ -9,6 +17,22 @@ class SetLinear(Token):
 
     Sets linear/circular mode to linear.
     """
+
+    def update_drawing_state(
+        self,
+        state: State,
+        _backend: Backend,
+    ) -> Tuple[State, Iterable[DrawAction]]:
+        """Set drawing mode."""
+        return (
+            state.model_copy(
+                update={
+                    "draw_mode": DrawMode.Linear,
+                },
+                deep=True,
+            ),
+            (),
+        )
 
     def __str__(self) -> str:
         """Return pretty representation of comment token."""
@@ -21,6 +45,22 @@ class SetClockwiseCircular(Token):
     Sets linear/circular mode to clockwise circular.
     """
 
+    def update_drawing_state(
+        self,
+        state: State,
+        _backend: Backend,
+    ) -> Tuple[State, Iterable[DrawAction]]:
+        """Set drawing mode."""
+        return (
+            state.model_copy(
+                update={
+                    "draw_mode": DrawMode.ClockwiseCircular,
+                },
+                deep=True,
+            ),
+            (),
+        )
+
     def __str__(self) -> str:
         """Return pretty representation of comment token."""
         return "G02*"
@@ -31,6 +71,22 @@ class SetCounterclockwiseCircular(Token):
 
     Sets linear/circular mode to counterclockwise circular.
     """
+
+    def update_drawing_state(
+        self,
+        state: State,
+        _backend: Backend,
+    ) -> Tuple[State, Iterable[DrawAction]]:
+        """Set drawing mode."""
+        return (
+            state.model_copy(
+                update={
+                    "draw_mode": DrawMode.CounterclockwiseCircular,
+                },
+                deep=True,
+            ),
+            (),
+        )
 
     def __str__(self) -> str:
         """Return pretty representation of comment token."""
