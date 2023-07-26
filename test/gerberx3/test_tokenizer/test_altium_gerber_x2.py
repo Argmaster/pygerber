05d325f800654e1191ed26ead04b7335415d61c4
Krzysztof Wisniewski <argmaster.world@gmail.com>
2023-07-26 18:16:39 +0200

    Add tokenizer tests for AltiumGerberX2 and KicadGerberX2 assets

---
+++ test/gerberx3/test_tokenizer/test_altium_gerber_x2.py
@@ -0,0 +1,28 @@
+"""Tokenizer tests based on AltiumGerberX2 board."""
+
+from __future__ import annotations
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+
+from test.gerberx3.common import (
+    find_gerberx3_asset_files,
+    save_token_stack,
+    tokenize_gerberx3,
+)
+
+if TYPE_CHECKING:
+    from test.conftest import AssetLoader
+
+
+@pytest.mark.parametrize(
+    ["directory", "file_name"],
+    sorted(find_gerberx3_asset_files("test/assets/gerberx3/AltiumGerberX2")),
+)
+def test_sample(asset_loader: AssetLoader, directory: Path, file_name: str) -> None:
+    """Test tokenizer on sample gerber code."""
+    stack = tokenize_gerberx3(asset_loader, directory, file_name)
+    save_token_stack(stack, __file__, directory, file_name)
