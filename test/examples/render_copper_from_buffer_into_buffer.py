2398f2945c64c81371494470790ba969ffba9c2b
Krzysztof Wisniewski <argmaster.world@gmail.com>
2023-08-31 10:12:44 +0200

    Add example tests for saving into buffer=

---
+++ test/examples/render_copper_from_buffer_into_buffer.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from io import BytesIO
+
+from pygerber.gerberx3.api import (
+    ColorScheme,
+    Rasterized2DLayer,
+    Rasterized2DLayerParams,
+)
+
+
+def render() -> None:
+    source_buffer = BytesIO(
+        b"""
+    %FSLAX26Y26*%
+    %MOMM*%
+    %ADD100R,1.5X1.0X0.5*%
+    %ADD200C,1.5X1.0*%
+    %ADD300O,1.5X1.0X0.6*%
+    %ADD400P,1.5X3X5.0*%
+    D100*
+    X0Y0D03*
+    D200*
+    X0Y2000000D03*
+    D300*
+    X2000000Y0D03*
+    D400*
+    X2000000Y2000000D03*
+    M02*
+    """,
+    )
+
+    output = BytesIO()
+    Rasterized2DLayer(
+        options=Rasterized2DLayerParams(
+            source_buffer=source_buffer,
+            colors=ColorScheme.COPPER_ALPHA,
+        ),
+    ).render().save(output, format="png")
+
+    output.seek(0)
+    content = output.read()
+    assert len(content) > 0
+
+
+if __name__ == "__main__":
+    render()
--- test/examples/test_examples.py
+++ test/examples/test_examples.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 from test.examples import (
     render_copper_from_buffer,
+    render_copper_from_buffer_into_buffer,
     render_copper_from_path,
+    render_copper_from_path_into_buffer,
     render_copper_from_string,
+    render_copper_from_string_into_buffer,
 )
 
 
@@ -17,3 +20,15 @@ def test_render_copper_from_string() -> None:
 
 def test_render_copper_from_path() -> None:
     render_copper_from_path.render()
+
+
+def test_render_copper_from_buffer_into_buffer() -> None:
+    render_copper_from_buffer_into_buffer.render()
+
+
+def test_render_copper_from_string_into_buffer() -> None:
+    render_copper_from_string_into_buffer.render()
+
+
+def test_render_copper_from_path_into_buffer() -> None:
+    render_copper_from_path_into_buffer.render()
