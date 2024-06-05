bbf5ff9c489cac19a90f68b8bf5172cda762ad59
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2024-06-06 00:44:32 +0200

    Remove old asset based Parser2 tests

--- test/gerberx3/test_parser2/common.py
+++ test/gerberx3/test_parser2/common.py
@@ -2,18 +2,14 @@
 
 from __future__ import annotations
 
-from pathlib import Path
-from test.gerberx3.common import find_gerberx3_asset_files
-from typing import TYPE_CHECKING, Callable
-
-import pytest
+from typing import TYPE_CHECKING
 
 from pygerber.gerberx3.parser2.context2 import Parser2Context
 from pygerber.gerberx3.parser2.parser2 import Parser2, Parser2Options
 from pygerber.gerberx3.tokenizer.tokenizer import Tokenizer
 
 if TYPE_CHECKING:
-    from test.conftest import AssetLoader
+    from pathlib import Path
 
 
 def debug_dump_context(ctx: Parser2Context, dest_dir: Path) -> None:
@@ -40,74 +36,6 @@ def debug_dump_context(ctx: Parser2Context, dest_dir: Path) -> None:
             buffer.get_readonly().debug_buffer_to_json(),
             encoding="utf-8",
         )
-
-
-def parse2(
-    asset_loader: AssetLoader,
-    src: str,
-    dest: Path,
-    *,
-    expression: bool = False,
-) -> None:
-    """Tokenize gerber code and save debug output."""
-    source = asset_loader.load_asset(src).decode("utf-8")
-    if expression:
-        stack = Tokenizer().tokenize_expressions(source)
-    else:
-        stack = Tokenizer().tokenize(source)
-
-    parser = Parser2()
-    parser.parse(stack)
-
-    debug_dump_context(parser.context, dest)
-
-
-def make_parser2_test(
-    test_file_path: str,
-    path_to_assets: str,
-    *,
-    expression: bool = False,
-) -> Callable[..., None]:
-    """Create parametrized test case for all files from path_to_assets.
-
-    All Gerber files from `path_to_assets` will be included as separate test cases
-    thanks to use of `@pytest.mark.parametrize`.
-
-    Parameters
-    ----------
-    test_file_path : str
-        Path to test file, simply use `__file__` variable from module global scope.
-    path_to_assets : str
-        Path to assets directory, originating from the root of repository, eg.
-        `test/assets/gerberx3/basic`.
-
-    Returns
-    -------
-    Callable[..., None]
-        Test callable. Must be assigned to variable with name starting with `test_`.
-    """
-    image_dump = Path(test_file_path).parent / ".output"
-
-    @pytest.mark.parametrize(
-        ("directory", "file_name"),
-        sorted(find_gerberx3_asset_files(path_to_assets)),
-    )
-    def test_sample(
-        asset_loader: AssetLoader,
-        directory: str,
-        file_name: str,
-    ) -> None:
-        """Rasterized2D rendering test based on sample files."""
-        dest = image_dump / directory / Path(file_name).with_suffix("")
-        dest.mkdir(mode=0o777, parents=True, exist_ok=True)
-        parse2(
-            asset_loader,
-            f"gerberx3/{directory}/{file_name}",
-            dest,
-            expression=expression,
-        )
-
-    return test_sample
 
 
 def parse_code(
--- test/gerberx3/test_parser2/test_altium_gerber_x2.py
+++
@@ -1,7 +0,0 @@
-"""tests based on AltiumGerberX2 board."""
-
-from __future__ import annotations
-
-from test.gerberx3.test_parser2.common import make_parser2_test
-
-test_sample = make_parser2_test(__file__, "test/assets/gerberx3/AltiumGerberX2")
