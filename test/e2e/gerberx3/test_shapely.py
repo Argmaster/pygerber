beb1bec573fceed5917bfd260b45f2c4d8e2b373
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2024-10-05 06:33:49 +0200

    Extend E2E test suite

--- test/e2e/gerberx3/test_shapely.py
+++ test/e2e/gerberx3/test_shapely.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 
 import inspect
 from pathlib import Path
-
-import pytest
+from typing import Type
 
 from pygerber.gerber.ast.nodes.file import File
 from pygerber.gerber.compiler import compile
@@ -16,10 +15,16 @@ from test.assets.generated.macro import (
     get_custom_circle_local_2_0_rot_30,
 )
 from test.assets.gerberx3.A64_OLinuXino_rev_G import A64_OlinuXino_Rev_G
+from test.assets.gerberx3.arc.clockwise import ClockwiseArcAssets
+from test.assets.gerberx3.arc.counterclockwise import CounterClockwiseArcAssets
+from test.assets.gerberx3.ATMEGA328 import ATMEGA328Assets
 from test.assets.gerberx3.FcPoly_Test import FcPoly_Test
 from test.assets.gerberx3.flashes import Flashes
 from test.assets.gerberx3.flashes_with_transform import FlashesWithTransform
+from test.assets.gerberx3.KicadGerberX2 import KiCadGerberX2Assets
 from test.assets.gerberx3.macro.codes import MacroCodeAssets
+from test.assets.gerberx3.polarity_cutouts import PolarityCutouts
+from test.assets.gerberx3.step_and_repeat import StepAndRepeatAssets
 from test.assets.gerberx3.ucamco import GerberSpecExampleAssets
 
 THIS_FILE = Path(__file__)
@@ -53,7 +58,6 @@ class ShapelyRenderE2E:
 
 
 class TestOLinuXinoRevG(ShapelyRenderE2E):
-    @pytest.mark.skip("Poor performance skip")
     def test_bottom_copper(self) -> None:
         result = self._render(A64_OlinuXino_Rev_G.A64_OlinuXino_Rev_G_B_Cu)
         self._save(result)
@@ -271,6 +275,70 @@ class TestMacroCodes(ShapelyRenderE2E):
         self._save(result)
 
 
+class ArcSuite(ShapelyRenderE2E):
+    assets: Type[ClockwiseArcAssets] | Type[CounterClockwiseArcAssets]
+
+    def test_full(self) -> None:
+        result = self._render(self.assets.full)
+        self._save(result)
+
+    def test_bot_half(self) -> None:
+        result = self._render(self.assets.half_bot)
+        self._save(result)
+
+    def test_top_half(self) -> None:
+        result = self._render(self.assets.half_top)
+        self._save(result)
+
+    def test_left_half(self) -> None:
+        result = self._render(self.assets.half_left)
+        self._save(result)
+
+    def test_right_half(self) -> None:
+        result = self._render(self.assets.half_right)
+        self._save(result)
+
+    def test_quarter_bot_left(self) -> None:
+        result = self._render(self.assets.quarter_bot_left)
+        self._save(result)
+
+    def test_quarter_bot_right(self) -> None:
+        result = self._render(self.assets.quarter_bot_right)
+        self._save(result)
+
+    def test_quarter_top_left(self) -> None:
+        result = self._render(self.assets.quarter_top_left)
+        self._save(result)
+
+    def test_quarter_top_right(self) -> None:
+        result = self._render(self.assets.quarter_top_right)
+        self._save(result)
+
+    def test_three_fourth_bot_left(self) -> None:
+        result = self._render(self.assets.three_fourth_bot_left)
+        self._save(result)
+
+    def test_three_fourth_bot_right(self) -> None:
+        result = self._render(self.assets.three_fourth_bot_right)
+        self._save(result)
+
+    def test_three_fourth_top_left(self) -> None:
+        result = self._render(self.assets.three_fourth_top_left)
+        self._save(result)
+
+    def test_three_fourth_top_right(self) -> None:
+        result = self._render(self.assets.three_fourth_top_right)
+        self._save(result)
+
+
+class TestMqClockwiseArcs(ArcSuite):
+    assets = ClockwiseArcAssets
+
+
+class TestMqCounterClockwiseArcs(ArcSuite):
+    assets = CounterClockwiseArcAssets
+
+
 class TestGerberSpecExampleAssets(ShapelyRenderE2E):
     def test_asset_2_1(self) -> None:
         result = self._render(GerberSpecExampleAssets.asset_2_1)
@@ -338,4 +406,150 @@ class TestGerberSpecExampleAssets(ShapelyRenderE2E):
 
     def test_asset_4_11_4(self) -> None:
         result = self._render(GerberSpecExampleAssets.asset_4_11_4)
+        self._save(result)
+
+
+class TestATMEGA328(ShapelyRenderE2E):
+    def test_bottom_copper(self) -> None:
+        result = self._render(ATMEGA328Assets.bottom_copper)
+        self._save(result)
+
+    def test_bottom_mask(self) -> None:
+        result = self._render(ATMEGA328Assets.bottom_mask)
+        self._save(result)
+
+    def test_bottom_paste(self) -> None:
+        result = self._render(ATMEGA328Assets.bottom_paste)
+        self._save(result)
+
+    def test_bottom_silk(self) -> None:
+        result = self._render(ATMEGA328Assets.bottom_silk)
+        self._save(result)
+
+    def test_edge_cuts(self) -> None:
+        result = self._render(ATMEGA328Assets.edge_cuts)
+        self._save(result)
+
+    def test_top_copper(self) -> None:
+        result = self._render(ATMEGA328Assets.top_copper)
+        self._save(result)
+
+    def test_top_mask(self) -> None:
+        result = self._render(ATMEGA328Assets.top_mask)
+        self._save(result)
+
+    def test_top_paste(self) -> None:
+        result = self._render(ATMEGA328Assets.top_paste)
+        self._save(result)
+
+    def test_top_silk(self) -> None:
+        result = self._render(ATMEGA328Assets.top_silk)
+        self._save(result)
+
+
+class TestPolarityCutouts(ShapelyRenderE2E):
+    def test_sample(self) -> None:
+        result = self._render(PolarityCutouts.sample)
+        self._save(result)
+
+
+class TestKiCadGerberX2(ShapelyRenderE2E):
+    def test_bottom_copper(self) -> None:
+        result = self._render(KiCadGerberX2Assets.bottom_copper)
+        self._save(result)
+
+    def test_bottom_mask(self) -> None:
+        result = self._render(KiCadGerberX2Assets.bottom_mask)
+        self._save(result)
+
+    def test_edge_cuts(self) -> None:
+        result = self._render(KiCadGerberX2Assets.edge_cuts)
+        self._save(result)
+
+    def test_top_copper(self) -> None:
+        result = self._render(KiCadGerberX2Assets.top_copper)
+        self._save(result)
+
+    def test_top_mask(self) -> None:
+        result = self._render(KiCadGerberX2Assets.top_mask)
+        self._save(result)
+
+    def test_top_paste(self) -> None:
+        result = self._render(KiCadGerberX2Assets.top_paste)
+        self._save(result)
+
+    def test_top_silk(self) -> None:
+        result = self._render(KiCadGerberX2Assets.top_silk)
+        self._save(result)
+
+
+class TestStepAndRepeat(ShapelyRenderE2E):
+    def test_cr_x_3(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_x_3)
+        self._save(result)
+
+    def test_cr_x_6(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_x_6)
+        self._save(result)
+
+    def test_cr_y_3(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_y_3)
+        self._save(result)
+
+    def test_cr_y_6(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_y_6)
+        self._save(result)
+
+    def test_cr_xy_3_6(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_xy_3_6)
+        self._save(result)
+
+    def test_cr_xy_6_6(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_xy_6_6)
+        self._save(result)
+
+    # Rot 30
+
+    def test_cr_x_3_rot_30(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_x_3_rot_30)
+        self._save(result)
+
+    def test_cr_x_6_rot_30(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_x_6_rot_30)
+        self._save(result)
+
+    def test_cr_y_3_rot_30(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_y_3_rot_30)
+        self._save(result)
+
+    def test_cr_y_6_rot_30(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_y_6_rot_30)
+        self._save(result)
+
+    def test_cr_xy_3_6_rot_30(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_xy_3_6_rot_30)
+        self._save(result)
+
+    def test_cr_xy_6_6_rot_30(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_xy_6_6_rot_30)
+        self._save(result)
+
+    # Line
+
+    def test_cr_xy_2_2_line(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_xy_2_2_line)
+        self._save(result)
+
+    def test_cr_xy_2_2_line_rot_30(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_xy_2_2_line_rot_30)
+        self._save(result)
+
+    # AB
+
+    def test_cr_xy_2_2_ab(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_xy_2_2_ab)
+        self._save(result)
+
+    def test_cr_xy_2_2_ab_rot_30(self) -> None:
+        result = self._render(StepAndRepeatAssets.cr_xy_2_2_ab_rot_30)
         self._save(result)
