fa4777dd996227c005ac740f7ebdbba659852141
Krzysztof Wiśniewski <argmaster.world@gmail.com>
2024-10-12 16:49:23 +0200

    Remove legacy error types from API

--- src/pygerber/gerber/api/_errors.py
+++ src/pygerber/gerber/api/_errors.py
@@ -11,20 +11,3 @@ class GerberX3APIError(Exception):
     `#!python try: ... except GerberX3APIError: ...` block to catch all exceptions
     raised by this API while allowing other exceptions to interrupt execution.
     """
-
-
-class RenderingResultNotReadyError(GerberX3APIError):
-    """Raised when RenderingResult is requested before it was rendered.
-
-    `Layer.get_rendering_result()` method can only be called after `Layer.render()`.
-    Breaking this rule will cause this exception to be raised.
-    """
-
-
-class MutuallyExclusiveViolationError(GerberX3APIError):
-    """Raised when two or more of mutually exclusive parameters are provided.
-
-    `LayerParams` class accepts three mutually exclusive fields, `source_path`,
-    `source_code` and `source_buffer` for providing source code to `Layer`.
-    When more than one of those options is set, this exception will be raised.
-    """
